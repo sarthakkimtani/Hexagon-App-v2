5006d67c6c27a7690642d77a438857ebb6a67c66
Sarthak Kimtani <sarthakkimtani123@gmail.com>
2020-11-24 19:03:03 +0530

    titlebar changes

--- public/electron.js
+++ public/electron.js
@@ -1,51 +1,54 @@
 // Modules to control application life and create native browser window
-const electron = require('electron');
-const { app, BrowserWindow, Menu } = require('electron');
+const electron = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 
-const path = require('path');
-const url = require('url');
-const isDev = require('electron-is-dev');
+const path = require("path");
+const isDev = require("electron-is-dev");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
-let mainMenu = Menu.buildFromTemplate(require('./mainMenu.js'));
+let mainMenu = Menu.buildFromTemplate(require("./mainMenu.js"));
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-  	width: 980,
-  	height: 695,
-	  minWidth: 980,
-	  minHeight: 695,
-	  maxWidth: 980,
-	  maxHeight: 695,
-	  maximizable: false,	
-  	title: "Hexagon - A Wallpaper App",
+    width: 980,
+    height: 695,
+    minWidth: 980,
+    minHeight: 695,
+    maxWidth: 980,
+    maxHeight: 695,
+    maximizable: false,
+    title: "Hexagon - A Wallpaper App",
     webPreferences: {
-      nodeIntegration: "true"
-    }
+      nodeIntegration: "true",
+    },
   });
-  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
-  mainWindow.on('closed', () => mainWindow = null);
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+  mainWindow.on("closed", () => (mainWindow = null));
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', () => {
+app.on("ready", () => {
   Menu.setApplicationMenu(mainMenu);
-  createWindow()
+  createWindow();
 });
 
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (process.platform !== 'darwin') {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (mainWindow === null) {
     createWindow();
   }
--- src/components/App.js
+++ src/components/App.js
@@ -3,26 +3,26 @@ import ErrorBoundary from "./ErrorBoundary";
 import HomePage from "./HomePage";
 import AgreementPage from "./AgreementPage";
 import MainPage from "./MainPage";
-import { BrowserRouter, Route, Switch, withRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, withRouter } from "react-router-dom";
 
 class App extends React.Component {
-    // eslint-disable-next-line
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return(
-            <BrowserRouter>
-              <ErrorBoundary>
-                <Switch>
-                  <Route path="/main" component={withRouter(MainPage)} />
-                  <Route path="/agree" component={withRouter(AgreementPage)} />
-                  <Route exact path="/" component={withRouter(HomePage)} />
-                </Switch>
-              </ErrorBoundary>
-            </BrowserRouter>
-        );
-    }
+  // eslint-disable-next-line
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <BrowserRouter>
+        <ErrorBoundary>
+          <Switch>
+            <Route path="/main" component={withRouter(MainPage)} />
+            <Route path="/agree" component={withRouter(AgreementPage)} />
+            <Route exact path="/" component={withRouter(HomePage)} />
+          </Switch>
+        </ErrorBoundary>
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App; 
+export default App;
